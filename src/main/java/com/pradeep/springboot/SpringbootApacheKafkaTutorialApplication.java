fdc1d27cbe16e1a7bf9432f574308eab49e4a2bc
pradeep <pradeepkumarhe1989@gmail.com>
2022-06-02 23:03:26 +0530

    First Commit

---
+++ src/main/java/com/pradeep/springboot/SpringbootApacheKafkaTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.pradeep.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApacheKafkaTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApacheKafkaTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pradeep/springboot/SpringbootApacheKafkaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradeep.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApacheKafkaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
